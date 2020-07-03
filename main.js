e3567a06befe5411509f47bdbe6dcafbd1df0a05
tyelf22 <tyelf22@hotmail.com>
2020-07-03 11:39:45 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,44 @@
+/* HEADER  */
+console.log('connected')
+let navBtn = document.querySelector(".checkBtn")
+let navBtnTwo = document.querySelector(".checkBtnTwo")
+let contactBtn = document.querySelector(".contactBtn")
+
+let links = document.querySelector(".navLinks")
+
+//resizing window determines nav items to display
+window.addEventListener('resize', function(event){
+    let viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+    if (viewportWidth > 950) {
+        console.log('Wide viewport');
+        navBtn.style.display = 'none'
+        navBtnTwo.style.display = 'none'
+    } else {
+        navBtn.style.display = 'block'
+        navBtnTwo.style.display = 'none'
+        links.style.left = "-100%"
+        contactBtn.style.left = "-100%"
+
+    }
+  });
+
+
+navBtn.addEventListener('click', () => {
+    console.log("clicked")
+    navBtn.style.display = "none"
+    navBtnTwo.style.display = "block"
+
+    links.style.left = "0"
+    contactBtn.style.left = "50%"
+    
+})
+
+navBtnTwo.addEventListener('click', () => {
+    console.log("clickedExit")
+    navBtn.style.display = "block"
+    navBtnTwo.style.display = "none"
+
+    links.style.left = "-100%"
+    contactBtn.style.left = "-100%"
+
+})
