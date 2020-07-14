cc108ed7552f164bda699aa3da192573edce039d
ColsonExplosion <tcolson12@gmail.com>
2020-07-14 14:02:58 -0600

    call now button

--- main.js
+++ main.js
@@ -42,3 +42,25 @@ navBtnTwo.addEventListener('click', () => {
     contactBtn.style.left = "-100%"
 
 })
+
+
+document.getElementById("name").focus();
+
+
+//NAV BAR
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+    }
+  }
